8a36f5f9f75852ae287b39887ee2c595a8d7bb61
Thinh Le <lqthinh93@gmail.com>
2019-05-16 23:11:23 +0700

    fix getting local token in request util

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
-let axiosHeaders = {
-  "Content-Type": "application/json"
-};
-
-try {
-  const localToken = localStorage.getItem("token");
-  if (localToken) {
-    axiosHeaders["authorization"] = localToken;
-  }
-} catch (err) {
-  console.log("parse token in requester err:", err);
-}
-
-const fetchInstance = axios.create({
-  baseURL: process.env.API_HOST || "http://localhost:8000",
-  headers: axiosHeaders
-});
-
 export default function request(options) {
+  let axiosHeaders = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      axiosHeaders["authorization"] = localToken;
+    }
+  } catch (err) {
+    console.log("parse token in requester err:", err);
+  }
+
+  const fetchInstance = axios.create({
+    baseURL: process.env.API_HOST || "http://localhost:8000",
+    headers: axiosHeaders
+  });
+
   return fetchInstance({
     ...options
   });
