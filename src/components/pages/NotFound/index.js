c95a287df6fef579876e8ccab1d5ba501c60c4a1
Thinh Le <lqthinh93@gmail.com>
2019-06-28 17:06:59 +0700

    modify not_found page

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import PublicLayout from "../../layouts/Public";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  msgContainer: {
+    textAlign: "center"
+  }
+});
 
 function NotFoundPage() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <PublicLayout>
+      <div className={classes.root}>
+        <div className={classes.msgContainer}>
+          <h1>
+            <strong>404</strong>
+          </h1>
+          <p>
+            <strong>Page not found</strong>
+          </p>
+          <p>
+            Back to <Link to="/">Home page</Link>
+          </p>
+        </div>
+      </div>
+    </PublicLayout>
   );
 }
 
--- src/containers/App/components/App.js
+++ src/containers/App/components/App.js
@@ -12,38 +12,48 @@ const App = ({ isValidated, ...rest }) => {
   if (!isValidated) return null;
   const { authUser } = rest;
   return (
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={props => <HomePage {...rest} {...props} />}
-      />
-      <Route
-        path="/signin"
-        render={props =>
-          validateAuth(!!authUser, false)(<SigninPage {...rest} {...props} />)
-        }
-      />
-      <Route
-        path="/signup"
-        render={props =>
-          validateAuth(!!authUser, false)(<SignupPage {...props} />)
-        }
-      />
-      <Route
-        path="/settings"
-        render={props =>
-          validateAuth(!!authUser, true)(<SettingsPage {...rest} {...props} />)
-        }
-      />
-      <Route
-        path="/messages/:id"
-        render={props =>
-          validateAuth(!!authUser, true)(<MessengerPage {...rest} {...props} />)
-        }
-      />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <React.Fragment>
+      <Switch>
+        {/* home */}
+        <Route
+          exact
+          path="/"
+          render={props => <HomePage {...rest} {...props} />}
+        />
+        {/* public routes */}
+        <Route
+          path="/signin"
+          render={props =>
+            validateAuth(!!authUser, false)(<SigninPage {...rest} {...props} />)
+          }
+        />
+        <Route
+          path="/signup"
+          render={props =>
+            validateAuth(!!authUser, false)(<SignupPage {...props} />)
+          }
+        />
+        {/* private routes */}
+        <Route
+          path="/settings"
+          render={props =>
+            validateAuth(!!authUser, true)(
+              <SettingsPage {...rest} {...props} />
+            )
+          }
+        />
+        <Route
+          path="/messages/:id"
+          render={props =>
+            validateAuth(!!authUser, true)(
+              <MessengerPage {...rest} {...props} />
+            )
+          }
+        />
+        {/* Not found */}
+        <Route component={NotFoundPage} />
+      </Switch>
+    </React.Fragment>
   );
 };
 
