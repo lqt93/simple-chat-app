8171e303d7805ea7220b449391f93544996ac124
Thinh Le <lqthinh93@gmail.com>
2019-07-08 22:50:47 +0700

    show back_button for chat_box room in mobile

--- src/containers/MessengerContainer/components/RoomContainer/RoomHeader.js
+++ src/containers/MessengerContainer/components/RoomContainer/RoomHeader.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { IconButton } from "@material-ui/core";
+import { KeyboardArrowLeft } from "@material-ui/icons";
 import { generateRoomName } from "../../utils/room";
 
 const useStyles = makeStyles(theme =>
@@ -11,6 +14,12 @@ const useStyles = makeStyles(theme =>
       alignItems: "center",
       padding: theme.spacing(1),
       borderBottom: "1px solid rgba(0, 0, 0, 0.2)"
+    },
+    backButton: {
+      display: "block",
+      [theme.breakpoints.up("sm")]: {
+        display: "none"
+      }
     }
   })
 );
@@ -19,6 +28,11 @@ const RoomHeader = ({ currentRoom, isOpenFindNameInput }) => {
   const classes = useStyles();
   return (
     <div className={classes.roomHeader}>
+      <Link to="/messenger">
+        <IconButton className={classes.backButton}>
+          <KeyboardArrowLeft />
+        </IconButton>
+      </Link>
       {currentRoom && !isOpenFindNameInput && (
         <strong>{generateRoomName(currentRoom)}</strong>
       )}
